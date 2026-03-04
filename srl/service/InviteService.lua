07339d2eb04fd099f23b0d72ad5988eb3f19bd3d
Aaron Alcoser <aalcoser85@gmail.com>
2026-03-03 23:57:18 -0600

    Class Fun.

---
+++ srl/service/InviteService.lua
@@ -0,0 +1,53 @@
+local mq = require('mq')
+
+local InviteService = {}
+
+function InviteService:init()
+
+    mq.event('GroupInvite', '#1# invites you to join a group.', function(inviter)
+        self:handleGroupInvite(inviter)
+    end)
+
+    mq.event('RaidInvite', '#1# invites you to join a raid.', function(inviter)
+        self:handleRaidInvite(inviter)
+    end)
+end
+
+function InviteService:handleRaidInvite(inviter)
+
+    if mq.TLO.Me.Raided() then return end
+
+    if not self:isDanNetPeer(inviter) then
+        print("Raid invite ignored (not a DanNet peer)")
+        return
+    end
+
+    mq.cmd('/raidinvite accept')
+
+end
+
+function InviteService:handleGroupInvite(inviter)
+
+    if mq.TLO.Me.Grouped() then return end
+
+    if not self:isDanNetPeer(inviter) then
+        print("Invite ignored (not a DanNet peer)")
+        return
+    end
+
+    mq.cmd('/invite')
+
+end
+
+function InviteService:isDanNetPeer(name)
+
+    local peers = mq.TLO.DanNet.Peers() or {}
+
+    for i=1, peers do
+        if mq.TLO.DanNet.Peers(i).Name() == name then
+            return true
+        end
+    end
+
+    return false
+end
